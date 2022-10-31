00d391060de3b05d331e6f67afcdda78b0d17938
Danny Bañol <dannybanol.dev@gmail.com>
2022-10-31 01:14:18 +0000

    removed sql dependency

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
